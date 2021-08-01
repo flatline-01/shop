3bfa54fb42b976de96616ae937b58a4454867204
I-am-Craz <thhiffxuui@gmail.com>
2021-08-01 23:42:57 +0600

    add a list of products with the same category

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,8 +1,19 @@
 const goodService = require("../services/good_service");
+const categoryService = require("../services/category_service");
+
 
 module.exports.singleProduct = async (req, resp) => {
     let productId = req.params.id;
     let product =  await goodService.checkProductId(productId);
+
+    const categoryId = product.categoryId;
+
+    let category = await categoryService.checkCategoryId(categoryId);
+    category.goods = await categoryService.checkCategoryGoods(categoryId);
+
+    product.categoryName = category.name;
+
+    product.otherCategoryGoods =  category.goods
 
     resp.render("product.pug", { product });
 };
